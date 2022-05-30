fb2df277f6cff4ae829b1c065ee7a5ed9fdcf5d0
gustavo <gustavohms1998@gmail.com>
2022-05-30 12:20:29 -0300

    Footer Implementado

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
 import Navbar from './components/estaticos/navbar/Navbar';
 import './App.css';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   );
 }

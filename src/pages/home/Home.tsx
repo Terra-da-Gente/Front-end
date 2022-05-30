78341fb2a8ab0bf5f0075acb86b33ae25b12ece8
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-30 14:42:52 -0300

    swipper and contact page update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,5 @@
 import React, { useRef, useState } from "react";
 
-import "./Home.css";
-
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,27 +10,28 @@ import "swiper/css/pagination";
 // import required modules
 import { Pagination } from "swiper";
 
-
+import "./Home.css";
+import { Typography } from "@material-ui/core";
 
 function Home() {
     return (
         <>
-            <Swiper className="mySwiper" style={{ height: '90vh' }}
+            <Swiper className="mySwiper" style={{ height: '80vh' }}
                 modules={[Pagination]}
-                pagination={{clickable: true}}
+                pagination={{ clickable: true }}
             >
 
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
-        
+                <SwiperSlide className="img1">
+                    <Typography variant="h2">Terra da Gente</Typography>
+                </SwiperSlide>
+                <SwiperSlide className="img2">
+                    <Typography variant="h2">Produtos selecionados</Typography>
+                </SwiperSlide>
+                <SwiperSlide className="img3">
+                    <Typography variant="h2">Terra da Gente</Typography>
+                </SwiperSlide>
+            </Swiper>
+
         </>
     )
 }

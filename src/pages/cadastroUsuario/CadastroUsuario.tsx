4ebaefb852816a9af7c8e7156a38423c4945a768
alebs10 <alexandre2002.ab@gmail.com>
2022-06-02 10:55:44 -0300

    Tela de Cadastro Concluída (por enquanto)

---
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { Grid, TextField, Typography, Box, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import User from "../../models/User";
+import { cadastroUsuario } from "../../services/Service";
+import { useNavigate } from "react-router-dom";
+import './CadastroUsuario.css';
+
+
+function CadastroUsuario() {
+
+    let navigate = useNavigate();
+    const [confirmarSenha, setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: ''
+        })
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: ''
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            navigate("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        // Estrutura Condicional que verifica se as senhas batem e se a Senha tem mais de 8 caracteres
+        if (confirmarSenha === user.senha && user.senha.length >= 8) {
+
+            //Tenta executar o cadastro
+            try {
+                await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+                alert("Usuário cadastrado com sucesso")
+
+                //Se houver erro, pegue o Erro e retorna uma msg   
+            } catch (error) {
+
+                //Pode modificar a msg de acordo com o erro 
+                alert("Usuário já existe")
+            }
+        } else {
+            alert("Insira no minímo 8 caracteres na senha")
+
+            setUser({...user, senha: ""})
+            setConfirmarSenha("")
+        }
+    }
+
+    return (
+        <Grid container direction='row' justifyContent="center" alignItems="center">
+            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+                <Box paddingX={10}>
+                    <form onSubmit={onSubmit}>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="textos2">
+                            Cadastrar
+                        </Typography>
+
+                        <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="nome" label="nome" variant="outlined" name="nome" margin="normal" fullWidth required />
+
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="usuario" variant="outlined" name="usuario" margin="normal" fullWidth required />
+
+                        
+
+                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id="confirmarSenha" label="confirmarSenha" variant="outlined" name="confirmarSenha" margin="normal" type="password" fullWidth required />
+
+                        <Box marginTop={2} textAlign="center">
+                            <Link to='/login' className="text-decorator-none">
+                                <Button variant="contained" color="secondary" className="btnCancelar">
+                                    Cancelar
+                                </Button>
+                            </Link>
+
+                            <Button type='submit' variant="contained" color="secondary" className="btnCadastrar">
+                                Cadastrar
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+            </Grid>
+
+        </Grid>
+    );
+}

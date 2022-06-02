af8d269b405f12e4043ff790060b33aca7288626
daniel <gardeli96>
2022-06-02 10:39:05 -0300

    atualizado02

---
+++ src/services/Service.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://terradagente.herokuapp.com/'
+
+})
+
+export const cadastroUsuario = async (url:any, dados:any, setDados:any)=>{
+    const resposta = await api.post(url,dados)
+    setDados (resposta.data)
+} 
+
+

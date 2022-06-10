fd4bb6ddb9021d3fce39f04af0453b2f7dad9232
gustavo <gustavohms1998@gmail.com>
2022-06-10 11:59:23 -0300

    Logo de aba Alterado

--- src/components/produtos/deletarProduto/DeletarProduto.tsx
+++ src/components/produtos/deletarProduto/DeletarProduto.tsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import { Box, Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Produto from '../../../models/Produto';
+import { buscaId, deleteId } from '../../../services/Service';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import './DeletarProduto.css';
 
 function DeletarProduto() {
-  return (
-    <div>DeletarProduto</div>
-  )
+
+    let history = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    const [produto, setProduto] = useState<Produto>()
+
+    useEffect(() => {
+        if (token == "") {
+            // toast.error('Você precisa estar logado!', {
+            //     position: "top-right",
+            //     autoClose: 5000,
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     pauseOnHover: true,
+            //     draggable: true,
+            //     progress: undefined,
+            //     theme: "dark"
+              // });
+            history("/login")
+
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/produto/${id}`, setProduto, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function sim() {
+        history('/produtos')
+        deleteId(`/produto/${id}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+
+        // toast.success('Produto deletada com sucesso !', {
+        //     position: "top-right",
+        //     autoClose: 5000,
+        //     hideProgressBar: false,
+        //     closeOnClick: true,
+        //     pauseOnHover: true,
+        //     draggable: true,
+        //     progress: undefined,
+        //     theme: "dark"
+        //   });
+        alert('Postagem deletada com sucesso');
+    }
+
+    function nao() {
+        history('/produtos')
+    }
+
+    return (
+        <>
+            <Box m={2}>
+                <Card variant="outlined" >
+                    <CardContent>
+                        <Box justifyContent="center">
+                            <Typography color="textSecondary" gutterBottom>
+                                Deseja deletar o Produto:
+                            </Typography>
+                            <Typography color="textSecondary" >
+                                {produto?.nome}
+                            </Typography>
+                        </Box>
+
+                    </CardContent>
+                    <CardActions>
+                        <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
+                            <Box mx={2}>
+                                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
+                                    Sim
+                                </Button>
+                            </Box>
+                            <Box>
+                                <Button onClick={nao} variant="contained" size='large' color="secondary">
+                                    Não
+                                </Button>
+                            </Box>
+                        </Box>
+                    </CardActions>
+                </Card>
+            </Box>
+        </>
+    )
 }
 
-export default DeletarProduto
+export default DeletarProduto;
--- src/components/produtos/listaProduto/ListaProduto.tsx
+++ src/components/produtos/listaProduto/ListaProduto.tsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import './ListaProduto.css';
+
+
+import { busca } from '../../../services/Service';
+import Produto from '../../../models/Produto';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
 
 function ListaProduto() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [produtos, setProdutos] = useState<Produto[]>([])
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    let navigate = useNavigate();
+
+    // useEffect(() => {
+    //     if (token == "") {
+    //         toast.error('VocÃª precisa estar logado!', {
+    //             position: "top-right",
+    //             autoClose: 5000,
+    //             hideProgressBar: false,
+    //             closeOnClick: true,
+    //             pauseOnHover: true,
+    //             draggable: true,
+    //             progress: undefined,
+    //             theme: "dark"
+    //           });
+    //         navigate("/login")
+
+    //     }
+    // }, [token])
+
+    async function getProduto() {
+        await busca("/produto", setProdutos, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+
+        getProduto()
+
+    }, [produtos.length])
+
+
+    return (
+        <>
+            {
+                produtos.map(produtos => (
+                    <Box m={2} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Produtos
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                {produtos.nome}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                {produtos.descricao}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                {produtos.categoria?.nome}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                                    <Link to={`/formularioProduto/${produtos.id}`} className="text-decorator-none" >
+                                        <Box mx={1}>
+                                            <Button variant="contained" className="marginLeft button-atualizar" size='small' color="primary" >
+                                                atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`/deletarProduto/${produtos.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button variant="contained" size='small' color="secondary" className='button-deletar'>
+                                                deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                </Box>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))
+            }
+        </>)
 }
 
-export default ListaProduto
+export default ListaProduto;

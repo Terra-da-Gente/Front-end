3db7491d42070d477bfaee3ae88384d73f99d800
gustavo <gustavohms1998@gmail.com>
2022-06-10 14:03:27 -0300

    Att

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,165 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import './Navbar.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { addToken } from '../../../store/tokens/actions';
+
+function Navbar() {
+
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  )
+
+  let history = useNavigate();
+
+  const dispatch = useDispatch();
+
+  function goLogout() {
+    dispatch(addToken(''));
+    history("/login")
+  }
+
+  var navbarComponent;
+
+  if (token === "") {
+    navbarComponent = <AppBar position="static" className="back" >
+      <Toolbar variant="dense" className="font">
+        <Box className="cursor" >
+          <Typography variant="h6" color="inherit" className="cursor" >
+            Terra da Gente
+          </Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="start">
+
+          <Link to='/home' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Home
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/produtos' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Produtos
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/sobre' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Sobre Nós
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/categoria' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Categorias
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/login' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Login
+              </Typography>
+            </Box>
+          </Link>
+
+        </Box>
+
+      </Toolbar>
+    </AppBar>
+  } else {
+    navbarComponent = <AppBar position="static" className="back" >
+      <Toolbar variant="dense" className="font">
+        <Box className="cursor" >
+          <Typography variant="h6" color="inherit" className="cursor" >
+            Terra da Gente
+          </Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="start">
+
+          <Link to='/home' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Home
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/produtos' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Produtos
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/sobre' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Sobre Nós
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/categoria' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Categorias
+              </Typography>
+            </Box>
+          </Link>
+
+
+          <Link to='/formularioCategoria' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Cadastrar Categoria
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/formularioProduto' className="text-decorator-none">
+            <Box mx={1} className="cursor ">
+              <Typography variant="h6" color="inherit">
+                Cadastrar Produto
+              </Typography>
+            </Box>
+          </Link>
+
+          <Link to='/home' className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Logout
+              </Typography>
+            </Box>
+          </Link>
+
+        </Box>
+
+      </Toolbar>
+    </AppBar>
+  }
+
+  return (
+    <>
+      {navbarComponent}
+    </>
+  );
+}
+
+export default Navbar;

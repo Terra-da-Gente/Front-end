70be19d37c4e030922e3441a2d275504765272b1
alebs10 <alexandre2002.ab@gmail.com>
2022-06-13 14:14:03 -0300

    Toast e UseEfeect inserido.

--- src/components/categoria/listaCategoria/ListaCategoria.tsx
+++ src/components/categoria/listaCategoria/ListaCategoria.tsx
@@ -1,23 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaCategoria.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { busca } from '../../../services/Service';
 import Categoria from '../../../models/Categoria';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
 
 function ListaCategoria() {
   const [categoria, setCategoria] = useState<Categoria[]>([])
-  
+
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
   )
 
   let history = useNavigate();
 
-  async function getCategoria(){
+  useEffect(() => {
+    if (token == "") {
+      toast.error('Você precisa estar logado!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
+      history("/login")
+
+    }
+  }, [token])
+
+  async function getCategoria() {
     await busca("/categoria", setCategoria, {
       headers: {
         'Authorization': token
@@ -25,46 +43,46 @@ function ListaCategoria() {
     })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getCategoria()
   }, [categoria.length])
 
   return (
     <>
-    {
-      categoria.map(categoria =>(
-      <Box m={2} >
-        <Card variant="outlined">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Categoria
-            </Typography>
-            <Typography variant="h5" component="h2">
-             {categoria.nome}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5} >
+      {
+        categoria.map(categoria => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Categoria
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {categoria.nome}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5} >
 
-              <Link to={`/formularioCategoria/${categoria.id}`} className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" className="button-atualizar" size='small'  >
-                    atualizar
-                  </Button>
+                  <Link to={`/formularioCategoria/${categoria.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" className="button-atualizar" size='small'  >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarCategoria/${categoria.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' className="button-deletar" >
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
                 </Box>
-              </Link>
-              <Link to={`/deletarCategoria/${categoria.id}`} className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" size='small' className="button-deletar" >
-                    deletar
-                  </Button>
-                </Box>
-              </Link>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
-      ))
+              </CardActions>
+            </Card>
+          </Box>
+        ))
       }
     </>
   );

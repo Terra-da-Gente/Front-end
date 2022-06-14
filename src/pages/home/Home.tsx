bec06899c7a1812c156910c4fbe97d44932e46de
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-14 18:12:09 -0300

    video update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -176,15 +176,15 @@ function Home() {
                 {/* ----- ITEM 5 ----- */}
                 < Grid item xs={6} style={{ height: '30vh', marginBottom: 300 }}>
                     <Box marginLeft={20}>
-                        <Typography align="left" variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}> VIDEO</Typography>
-                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>LOREM.</Typography>
-                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
+                        <Typography align="left" variant='h5' style={{ fontWeight: 'bold', letterSpacing: 4, lineHeight: 2 }}>Terra da Gente</Typography>
+                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>Terra da Gente</Typography>
+                        <Typography align="left" variant='h6' style={{ marginTop: 15 }}>O projeto Terra da Gente visa melhorar a qualidade de vida e fornecer meios de subsistência para as famílias em situação de assentamento envolvidas, bem como incentivar o consumo de produtos e serviços dos  pequenos empreendedores, agricultores rurais e urbanos em situação de vulnerabilidade.</Typography>
                     </Box>
                 </Grid>
 
                 <Grid item xs={6} >
                     <Box className="video" padding={10} style={{ height: '40vh', marginBottom: 300 }}>
-                        <iframe src="https://www.youtube.com/embed/B7HdmzRBT9U" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/sOHIQ1aW6FA" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
                     </Box>
                 </Grid>
             </Grid>

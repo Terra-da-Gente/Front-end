98222fdf66d348ccfa4641e6a94b9e8a335ad4b0
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-21 16:55:00 -0300

    title update

--- src/components/categoria/listaCategoriaProduto/ListaCategoriaProduto.tsx
+++ src/components/categoria/listaCategoriaProduto/ListaCategoriaProduto.tsx
@@ -161,7 +161,7 @@ function ListaCategoriaProduto() {
                 alignItems="center"
             >
                 <Grid item xs={12}>
-                    <Typography variant='h4' align='center' style={{color: 'black', marginTop: 40, marginBottom:20, letterSpacing: 6, textTransform: 'uppercase' }}>
+                    <Typography variant='h5' align='center' style={{color: 'black', marginTop: 50, marginBottom:20, letterSpacing: 6, textTransform: 'uppercase' }}>
                         {categorias.nome}
                     </Typography>
                 </Grid>

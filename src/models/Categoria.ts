add9245bfbf0f39e875d467c744150fb1e124a87
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-21 15:41:42 -0300

    busca de produto por categoria

--- src/models/Categoria.ts
+++ src/models/Categoria.ts
@@ -1,8 +1,11 @@
+import Produto from "./Produto"
+
 interface Categoria {
     id: number;
     nome: string;
     ativo: boolean;
     foto1: string;
+    produto?: Produto[] | null
 }
 
 export default Categoria;

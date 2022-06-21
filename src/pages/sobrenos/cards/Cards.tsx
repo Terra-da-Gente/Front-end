d0c38fd2e7f64273e0775d0ee0ce6f6874b83179
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-20 21:54:35 -0300

    bg and fontsize update

--- src/pages/sobrenos/cards/Cards.tsx
+++ src/pages/sobrenos/cards/Cards.tsx
@@ -6,147 +6,140 @@ import "./Cards.css";
 function Cards() {
   return (
     <Grid container direction="row">
-        <Grid item xs={2}>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <Grid item xs={2} >
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/wt7UemZ.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/wt7UemZ.png" />
           </div>
           <div className="team-content">
-            <h3 className="name">Alexandre Barbosa</h3>
+            <h3>Alexandre Barbosa</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social redes-sobre-nos">
             <li><a href="https://www.linkedin.com/in/alexandrebsilva/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/alebs10" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/alebs10" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
+
         </div>
-      </div>
       </Grid>
       <Grid item xs={2}>
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/F1QfUzU.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/F1QfUzU.png" />
           </div>
           <div className="team-content">
             <h3 className="name">Daniel Gardeli</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social">
-          <li><a href="https://www.linkedin.com/in/daniel-gardeli/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+            <li><a href="https://www.linkedin.com/in/daniel-gardeli/" target="_blank">
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/gardeli96" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/gardeli96" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
         </div>
-      </div>
       </Grid>
       <Grid item xs={2}>
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/SaAp6Zo.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/SaAp6Zo.png" />
           </div>
           <div className="team-content">
             <h3 className="name">Gustavo Henrique</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social">
-          <li><a href="https://www.linkedin.com/in/gustavohms1998/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+            <li><a href="https://www.linkedin.com/in/gustavohms1998/" target="_blank">
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/guhms1998" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/guhms1998" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
         </div>
-        </Grid>
+      </Grid>
       <Grid item xs={2}>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/WtctWE7.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/WtctWE7.png" />
           </div>
           <div className="team-content">
             <h3 className="name">Letícia Zuniga</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social">
-          <li><a href="https://www.linkedin.com/in/leticia-zuniga/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+            <li><a href="https://www.linkedin.com/in/leticia-zuniga/" target="_blank">
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/LeZuniga" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/LeZuniga" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
         </div>
-      </div>
       </Grid>
       <Grid item xs={2}>
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/O6TWCF8.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/O6TWCF8.png" />
           </div>
           <div className="team-content">
             <h3 className="name">Mateus Nogueira</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social">
-          <li><a href="https://www.linkedin.com/in/mateusnog95/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+            <li><a href="https://www.linkedin.com/in/mateusnog95/" target="_blank">
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/nogran" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/nogran" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
         </div>
-        </div>
-        </Grid>
+      </Grid>
       <Grid item xs={2}>
         <div className="our-team">
           <div className="picture">
-            <img className="img-fluid" src="https://i.imgur.com/jnVoxIU.png"/>
+            <img className="img-fluid" src="https://i.imgur.com/jnVoxIU.png" />
           </div>
           <div className="team-content">
             <h3 className="name">Tainã Xavier</h3>
             <h4 className="title">Web Developer</h4>
           </div>
           <ul className="social">
-          <li><a href="https://www.linkedin.com/in/tainaxsp/" target="_blank">
-                        <LinkedIn className="redes-sobre-nos" />
-                </a>
+            <li><a href="https://www.linkedin.com/in/tainaxsp/" target="_blank">
+              <LinkedIn className="redes-sobre-nos" />
+            </a>
             </li>
-            <li>                    
-                <a href="https://github.com/taaixsp" target="_blank">
-                        <GitHub className="redes-sobre-nos" />
-                </a>
+            <li>
+              <a href="https://github.com/taaixsp" target="_blank">
+                <GitHub className="redes-sobre-nos" />
+              </a>
             </li>
           </ul>
         </div>
-  </Grid>
-  </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
